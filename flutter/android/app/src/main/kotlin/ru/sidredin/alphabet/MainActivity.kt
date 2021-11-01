5e0c8dd2552476a50c0cc34cfd0d211fc598d241
sadr3479 <sadr3479@gmail.com>
2021-11-01 23:38:31 +0300

    Init Flutter project

---
+++ flutter/android/app/src/main/kotlin/ru/sidredin/alphabet/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.sidredin.alphabet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:alphabet/views/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UvU Driver Application',
+      theme: ThemeData(),
+      initialRoute: AuthScreen.routeName,
+      routes: {
+        AuthScreen.routeName: (context) => const AuthScreen(),
+        // ProfileScreen.routeName: (context) => const ProfileScreen(),
+      },
+    );
+  }
+}
